7097525d9f19e431d9191a5a1527fdbdc04ed43c
FengYan Lin <106728194+mlxger@users.noreply.github.com>
2025-07-31 21:08:49 +0800

    Update and rename TCN.py to TemporalConvNet.py

--- TemporalConvNet.py
+++ TemporalConvNet.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
 from torch.nn.utils import weight_norm
-# 这个函数是用来修剪卷积之后的数据的尺寸，让其与输入数据尺寸相同。
+
 class Chomp1d(nn.Module):
     """
-    :param chomp_size: int, 剪裁尺寸
+    :param chomp_size: int, 
     """
     def __init__(self, chomp_size):
         super(Chomp1d, self).__init__()
@@ -13,24 +13,17 @@ class Chomp1d(nn.Module):
     def forward(self, x):
         return x[:, :, :-self.chomp_size].contiguous()
  
-# 这个就是TCN的基本模块，包含8个部分，两个（卷积+修剪+relu+dropout）
-# 里面提到的downsample就是下采样，其实就是实现残差链接的部分。不理解的可以无视这个
+
 class TemporalBlock(nn.Module):
     """
-        构成TCN的核心Block, 原作者在图中成为Residual block, 是因为它存在残差连接.
-        但注意, 这个模块包含了2个Conv1d.
  
-        :param n_inputs: int, 输入通道数或者特征数
-        :param n_outputs: int, 输出通道数或者特征数
-        :param kernel_size: int, 卷积核尺寸
-        :param stride: int, 步长, 在TCN固定为1
-        :param dilation: int, 膨胀系数. 与这个Residual block(或者说, 隐藏层)所在的层数有关系. 
-                                例如, 如果这个Residual block在第1层, dilation = 2**0 = 1;
-                                      如果这个Residual block在第2层, dilation = 2**1 = 2;
-                                      如果这个Residual block在第3层, dilation = 2**2 = 4;
-                                      如果这个Residual block在第4层, dilation = 2**3 = 8 ......
-        :param padding: int, 填充系数. 与kernel_size和dilation有关. 
-        :param dropout: float, dropout比率
+        :param n_inputs: int, 
+        :param n_outputs: int, 
+        :param kernel_size: int, 
+        :param stride: int, 
+        :param dilation: int, 
+        :param padding: int, 
+        :param dropout: float, 
     """
     def __init__(self, n_inputs, n_outputs, kernel_size, stride, dilation, padding, dropout=0.2):
         super(TemporalBlock, self).__init__()
@@ -67,14 +60,10 @@ class TemporalConvNet(nn.Module):
     def __init__(self, num_inputs, num_channels, kernel_size=2, dropout=0.2):
         super(TemporalConvNet, self).__init__()
         """
-        :param num_inputs: int,  输入通道数或者特征数
-        :param num_channels: list, 每层的hidden_channel数. 例如[5,12,3], 代表有3个block, 
-                                block1的输出channel数量为5; 
-                                block2的输出channel数量为12;
-                                block3的输出channel数量为3.
-        :param kernel_size: int, 卷积核尺寸
-        :param dropout: float, drop_out比率
-        """
+        :param num_inputs: int, 
+        :param num_channels: list, 
+        :param kernel_size: int, 
+        :param dropout: float, 
         layers = []
         num_levels = len(num_channels)
         for i in range(num_levels):
